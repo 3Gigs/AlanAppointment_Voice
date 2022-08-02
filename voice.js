e41e7225a233348b12c8b92ecf169e0ea5f7c1a4
chenyinuo8888 <65636361+chenyinuo8888@users.noreply.github.com>
2022-08-01 21:07:26 -0700

    Guide response

--- voice.js
+++ voice.js
@@ -3,5 +3,9 @@ intent('hello', p => {
     p.play('(hello|hi there|hey)');
 });
 intent ('(guide|help|teach)', p=> {
-    p.play('This is a website intergrated with voice AI that can help you to manage your scheduals');
+   intro(p);
 });
+function intro (p)
+{ 
+    p.play('This is a website intergrated with voice AI that can help you to manage your schedules');
+}
