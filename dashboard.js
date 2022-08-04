a69f7faedf99fa56512aa6ad45081e0a129a813b
Huaxuan Yang <yanghuaxuan@gmail.com>
2022-08-04 01:04:24 -0700

    Update dashboard.js
    Fixed some typos, changed appointment intent and about context

--- dashboard.js
+++ dashboard.js
@@ -1,22 +1,24 @@
 const vDashboard = visual({screen: "Dashboard"});
 
 let about = context(() => {
-    intent('I want a $(app~ doctor appiontment~doctor|meeting~business meeting|due date ~ due date of|counseling~conversation|class~course)' , p => {
+    intent('I want a $(app~ doctor appointment~doctor|meeting~business meeting|due date ~ due date of|counseling~conversation|class~course)' , p => {
         p.play(`You have made an appiontment about ${p.app.label}`);
         p.play({ 
-            command : `appointmentLabel(${p.app.label})`
+            command : {title: p.app.label, start: p.state.start, end: p.state.end, id: null}
         });
     });
 });
 
 intent(vDashboard, ("Make an appointment on $(DATE)"), p => {
     p.play('make an appointment on ');
-    p.play(p.DATE.value); 
+    p.play(p.DATE.luxon.toISO()); 
     p.play({
-        command : `schedules(${p.DATE})`
+        command : `schedules(${p.DATE.luxon.toISO()})`
     });
+    p.state.start = p.DATE.luxon.toISO();
+    p.state.end = p.DATE.luxon.toISO();
     p.play('what you want to make an appointment for?');
-    p.then(about);
+    p.then(about, {state: p.state});
 });
 
 //Only day and time, default 1 hout appointment.
@@ -24,11 +26,8 @@ intent(vDashboard, ("Make an appointment on $(DATE) at $(TIME)"), p => {
     p.play('make an appointment on ');
     p.play(p.DATE.value); 
     p.play(`at ${p.TIME.value}`);
-    p.play({
-        command : "schedules($(p.DATE),$(p.TIME))"
-    });
     p.play('what you want to make an appointment for?');
-    p.then(about);
+    p.then(about, {state: p.state});
 });
 
 //Given starting time and end time
