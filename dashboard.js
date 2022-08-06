8135b41e25f763d6eff0675d2ddae5bc41864377
Huaxuan Yang <yanghuaxuan@gmail.com>
2022-08-05 23:06:16 -0700

    More enhancements

--- dashboard.js
+++ dashboard.js
@@ -2,20 +2,19 @@ const vDashboard = visual({screen: "Dashboard"});
 const vDashboardModal = visual({screen: "Dashboard", modal: true});
 
 let about = context(() => {
-    intent('I want a $(app~ doctor appointment~doctor|meeting~business meeting|due date ~ due date of|counseling~conversation|class~course)' , p => {
-        p.play(`You have made an appiontment about ${p.app.label}`);
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    intent('(I want a | It\' for) $(TITLE* .+)' , p => {
+        p.play(`You have made an appiontment about ${p.TITLE.value} on ${new Date(p.state.start).toLocaleDateString(undefined, dateOptions)} to ${new Date(p.state.end).toLocaleDateString(undefined, dateOptions)}`);
         p.play({ 
-            command:"createEvent", eventInfo : {title: p.app.label, start: p.state.start, end: p.state.end, id: null}
+            command:"createEvent", eventInfo : {title: p.TITLE.value, start: p.state.start, end: p.state.end, id: null}
         });
     });
 });
 
-intent(vDashboard, ("Make an appointment on $(DATE)"), p => {
-    p.play('make an appointment on ');
+
+intent(vDashboard, ("Make an appointment (on | at) $(DATE)"), p => {
     p.play(p.DATE.luxon.toISO()); 
-    p.play({
-        command : `schedules(${p.DATE.luxon.toISO()})`
-    });
     p.state.start = p.DATE.luxon.toISO();
     p.state.end = p.DATE.luxon.toISO();
     p.play('what you want to make an appointment for?');
@@ -23,27 +22,30 @@ intent(vDashboard, ("Make an appointment on $(DATE)"), p => {
 });
 
 //Only day and time, default 1 hout appointment.
-intent(vDashboard, ("Make an appointment on $(DATE) at $(TIME)"), p => {
-    p.play('make an appointment on ');
-    p.play(p.DATE.value); 
-    p.play(`at ${p.TIME.value}`);
-    p.play('what you want to make an appointment for?');
+intent(vDashboard, ("Make an appointment (on | at) $(DATE) at $(TIME)"), p => {
+    p.state.start = p.DATE.luxon.toISO();
+    p.state.end = p.DATE.luxon.toISO();
+
+    p.play('Ok, what you want to make an appointment for?');
     p.then(about, {state: p.state});
+});
+
+intent(vDashboard, ("Make an appointment (on | at) $(fromDate DATE) to $(toDate DATE)"), p => {
+   p.state.start = p.fromDate.luxon.toISO();
+   p.state.end = p.toDate.luxon.toISO();
+   p.play('Ok, what you want to make an appointment for?');
+   p.then(about, {state: p.state});
 });
 
 //Given starting time and end time
 intent(vDashboard, ("Make an appointment from $(fromDate DATE) $(fromTime TIME) to $(toDate DATE) $(toTime TIME)"), p => {
-    p.play('make an appointment from ');
-    p.play(p.fromDate.value); 
-    p.play(p.fromTime.value);
-    p.play("to");
-    p.play(p.toDate.value); 
-    p.play(p.toTime.value);
-    p.play({
-        command : `schedules(${p.fromDate},${p.fromTime},${p.toDate},${p.toTime})`
-    });
+    const startTime = p.fromDate.luxon.plus({seconds: p.fromTime.time}).toISO();
+    const endTime = p.toDate.luxon.plus({seconds: p.toTime.time}).toISO();
+    
+    p.state.start = startTime;
+    p.state.end = endTime;
     p.play('what you want to make an appointment for?');
-    p.then(about);
+    p.then(about, {state: p.state});
 });
 
 intent(vDashboardModal, "(close | got it | thank you)", p => {
